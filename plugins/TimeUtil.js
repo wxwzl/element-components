85995a942b87fccf651dc812961f73950a0fa076
wxwzl <zgkj456@163.com>
2020-07-25 15:37:50 +0800

    完善formatDate

--- plugins/TimeUtil.js
+++ plugins/TimeUtil.js
@@ -11,22 +11,27 @@ let TimeUtil = {
         addend = Math.floor(oneDayTime*addend);
         return new Date(date.getTime()+addend);
     },
-    formatDate(date){
-        if(date instanceof  Date !=true){
-            date= new Date(date);
+    formatDate(date,fmt) {
+        if(date instanceof  Date != true) {
+            date = new Date(date)
         }
-
-        let myyear = date.getFullYear(); 
-        let mymonth = date.getMonth()+1; 
-        let myweekday = date.getDate(); 
-        
-        if(mymonth < 10){ 
-        mymonth = "0" + mymonth; 
-        } 
-        if(myweekday < 10){ 
-             myweekday = "0" + myweekday; 
-        } 
-        return (myyear+"-"+mymonth + "-" + myweekday);  
+        if (!fmt) fmt = "yyyy-MM-dd";
+    
+        if (!date || date == null) return null;
+        var o = {
+        'M+': date.getMonth() + 1, // 月份
+        'd+': date.getDate(), // 日
+        'h+': date.getHours(), // 小时
+        'm+': date.getMinutes(), // 分
+        's+': date.getSeconds(), // 秒
+        'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+        'S': date.getMilliseconds() // 毫秒
+        }
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+        for (var k in o) {
+        if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+        }
+        return fmt;
     },
 
 };
